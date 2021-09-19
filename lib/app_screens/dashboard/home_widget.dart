582ea4256447382ab93cd46112710003222baf4d
nagendrainvweb <nagendra@inventifweb.com>
2021-09-19 15:27:31 +0530

    new_order added

--- lib/app_screens/dashboard/home_widget.dart
+++ lib/app_screens/dashboard/home_widget.dart
@@ -1,5 +1,7 @@
+import 'package:diamon_assorter/app_screens/new_order/newOrder_screen.dart';
 import 'package:diamon_assorter/util/app_color.dart';
 import 'package:diamon_assorter/util/app_image.dart';
+import 'package:diamon_assorter/util/utility.dart';
 import 'package:flutter/material.dart';
 
 class HomeWidget extends StatefulWidget {
@@ -10,52 +12,56 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
-  _buttonTiles( String text, String image) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-        vertical: 30,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                AppColors.mainDarkColor,
-                AppColors.mainLightColor
+  _buttonTiles(String text, String image, Function onClick) {
+    return InkWell(
+      onTap: onClick,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [AppColors.mainDarkColor, AppColors.mainLightColor],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Image.asset(
+                  image,
+                  height: 50,
+                  fit: BoxFit.cover,
+                ),
+                // Icon(
+                //   icons,
+                //   size: 45.0,
+                //   color: AppColors.whiteColor,
+                //   ),
+                Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: AppColors.whiteColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ],
             ),
-            boxShadow: [
-      BoxShadow(
-        color: Colors.grey.withOpacity(0.5),
-        spreadRadius: 3,
-        blurRadius: 7,
-        offset: Offset(0, 3), // changes position of shadow
-      ),
-    ],
-          ),
-      child: Column(
-        children: [
-          Column(
-            children: [
-              Image.asset(image,height: 50,fit: BoxFit.cover,),
-              // Icon(
-              //   icons,
-              //   size: 45.0,
-              //   color: AppColors.whiteColor,
-              //   ),
-              Text(
-                text,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: AppColors.whiteColor,
-                  fontWeight: FontWeight.bold,
-                ),
-                ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -63,7 +69,6 @@ class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-
       padding: EdgeInsets.symmetric(
         horizontal: 30,
       ),
@@ -72,55 +77,79 @@ class _HomeWidgetState extends State<HomeWidget> {
         context: context,
         child: ListView(
           children: [
-            SizedBox(height: 30,),
-            Row(
-              children: [
-                Expanded(child: _buttonTiles(
-                  "Upcoming\nOrders",
-                 AppImages.upcomingOrder,
-                ),),
-                SizedBox(width: 20,),
-                Expanded(child: _buttonTiles(
-                  "Pending\nOrders",
-                 AppImages.pendingOrder,
-                ),),
-                
-              ],
+            SizedBox(
+              height: 30,
             ),
-            SizedBox(height: 20,),
-            Row(
-              children: [
-                Expanded(
-                  child: 
-                  _buttonTiles(
-                  "Ongoing\nOrders",
-                  AppImages.ongoingOrder,
-                ),
-                  ),
-                  SizedBox(width: 20,),
-                Expanded(child:_buttonTiles(
-                  "New\nOrders",
-                  AppImages.newOrder,
-                ), ),
-                
-              ],
-            ),
-            SizedBox(height: 20,),
             Row(
               children: [
                 Expanded(
                   child: _buttonTiles(
-                  "Completed\nOrders",
-                  AppImages.completeOrder,
+                    "Upcoming\nOrders",
+                    AppImages.upcomingOrder,
+                    (){},
+                  ),
                 ),
+                SizedBox(
+                  width: 20,
                 ),
-                SizedBox(width: 20,),
-                Expanded(child: Container(),),
-                
+                Expanded(
+                  child: _buttonTiles(
+                    "Pending\nOrders",
+                    AppImages.pendingOrder,
+                    (){},
+                  ),
+                ),
               ],
             ),
-            SizedBox(height: 20,),
-
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: _buttonTiles(
+                    "Ongoing\nOrders",
+                    AppImages.ongoingOrder,
+                    (){},
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: _buttonTiles(
+                    "New\nOrders",
+                    AppImages.newOrder,
+                    () {
+                      Utility.pushToNext(context, NewOrderPage());
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: _buttonTiles(
+                    "Completed\nOrders",
+                    AppImages.completeOrder,
+                    (){},
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
           ],
         ),
       ),
